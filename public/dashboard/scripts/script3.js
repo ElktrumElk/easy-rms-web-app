b1431499d7e240bd42e195c0b8f1696a334c69ea
ElktrumElk <festinacole373@gmail.com>
2026-04-06 01:12:45 +0000

    light and dark theme

--- public/dashboard/scripts/script3.js
+++ public/dashboard/scripts/script3.js
@@ -21,7 +21,23 @@ export default function interSect() {
         })
         if (scrollCnt.scrollTop <= 0) {
             rootCnt.style.boxShadow = "0px 0px 0rem gray"
+        };
+    })
+
+    const tg_btn = document.getElementById('theme_tg_btn');
+
+    let isDark = false;
+
+    tg_btn.addEventListener("click", () => {
+        if (!isDark) {
+            document.body.classList.add("dark");
+            isDark = true;
+            tg_btn.src = "https://img.icons8.com/?size=100&id=648&format=png&color=ffffff";
+
+        } else {
+            document.body.classList.remove("dark");
+            isDark = false;
+            tg_btn.src = "https://img.icons8.com/?size=100&id=45474&format=png&color=7a7a7a";
         }
-        console.log(scrollCnt.scrollHeight, scrollCnt.scrollTop)
     })
 }
